e9c5d2465cd61dc6904612f662c8f2bcd40cbb1f
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-06-22 20:42:54 +0530

    Ready for deployment

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -47,7 +47,8 @@ const HomePage = () => {
       <div className="hero-section d-flex align-items-center justify-content-center text-center">
         <div className="overlay"></div>
        <div className="content text-white px-3 text-center">
-          <h1 className="fw-bold mb-3">Welcome to the TnP Recruitment App</h1>
+          <h1 className="fw-bold mb-3">Shaping Futures with Opportunities<br/>
+Training and Placement Cell, Delhi Technological University</h1>
           <p className="lead mb-4">
             Use the admin panel to generate shareable student data links securely.
           </p>
